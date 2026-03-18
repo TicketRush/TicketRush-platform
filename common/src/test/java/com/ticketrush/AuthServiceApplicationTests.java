de9b89125b775e13b506a1c7afb181429ff7967e
민주's mac <calla20031@Mac.local>
2026-03-18 14:08:56 +0900

    [Fix] #14 common 모듈의 부적절한 SpringBootTest 설정 제거

--- common/src/test/java/com/ticketrush/AuthServiceApplicationTests.java
+++ common/src/test/java/com/ticketrush/AuthServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.ticketrush;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AuthServiceApplicationTests {
 
   @Test
