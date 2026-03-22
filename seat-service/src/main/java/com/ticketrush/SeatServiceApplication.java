11f2f80787c5bd9fd8624709bcfffbbcb3bd6b38
sohee52 <wneh2023@naver.com>
2026-03-22 15:00:44 +0900

    [Feat] #28 seat-service 모듈 추가

---
+++ seat-service/src/main/java/com/ticketrush/SeatServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ticketrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SeatServiceApplication.class, args);
+  }
+}
---
+++ seat-service/src/test/java/com/ticketrush/SeatServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ticketrush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
