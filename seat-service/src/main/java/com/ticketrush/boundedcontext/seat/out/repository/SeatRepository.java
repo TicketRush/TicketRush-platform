129b80c3e1f96d48c58bb1ced6493a8b73a74e31
50h33 <wneh2023@naver.com>
2026-03-24 17:13:13 +0900

    [Feat] #29 좌석 배치 조회 포함된 SeatRepository 추가

---
+++ seat-service/src/main/java/com/ticketrush/boundedcontext/seat/out/repository/SeatRepository.java
@@ -0,0 +1,18 @@
+package com.ticketrush.boundedcontext.seat.out.repository;
+
+import com.ticketrush.boundedcontext.seat.domain.dto.response.SeatLayoutResponse;
+import com.ticketrush.boundedcontext.seat.domain.entity.Seat;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+  @Query("SELECT new com.ticketrush.boundedcontext.seat.domain.dto.response.SeatLayoutResponse(" +
+    "s.id, sl.id, sl.rowNo, sl.colNo) " +
+    "FROM Seat s " +
+    "JOIN SeatLayout sl ON s.seatLayoutId = sl.id " +
+    "WHERE s.performanceId = :performanceId")
+  List<SeatLayoutResponse> findSeatLayoutsByPerformanceId(@Param("performanceId") Long performanceId);
+}
