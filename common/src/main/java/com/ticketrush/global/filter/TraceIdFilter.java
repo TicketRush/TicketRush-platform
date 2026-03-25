04ee51cd0ae5e3b86c3f6863f188fb3e61e40a79
50h33 <wneh2023@naver.com>
2026-03-25 12:02:43 +0900

    [Chore] #52 사용하지 않는 @Slf4j 제거

--- common/src/main/java/com/ticketrush/global/filter/TraceIdFilter.java
+++ common/src/main/java/com/ticketrush/global/filter/TraceIdFilter.java
@@ -6,14 +6,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Slf4j
 @Component
 @Order(1)
 public class TraceIdFilter extends OncePerRequestFilter {
