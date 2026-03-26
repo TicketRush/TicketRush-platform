bbafe04cb732ae62a7545e5628b4f6d704ef6f4f
sohee52 <wneh2023@naver.com>
2026-03-27 02:52:15 +0900

    [Feat] #51 JSON 변환 실패 관련 커스텀 예외 추가

---
+++ common/src/main/java/com/ticketrush/global/json/SerializationException.java
@@ -0,0 +1,7 @@
+package com.ticketrush.global.json;
+
+public class SerializationException extends RuntimeException {
+  public SerializationException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
