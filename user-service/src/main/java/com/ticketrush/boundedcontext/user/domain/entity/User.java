741f5f8e3e348aa33e21919537d3787f65bd83ae
김혜림 <khrim981209@gmail.com>
2026-03-27 01:24:44 +0900

    [Feat] #61 LoginType 삭제

--- user-service/src/main/java/com/ticketrush/boundedcontext/user/domain/entity/User.java
+++ user-service/src/main/java/com/ticketrush/boundedcontext/user/domain/entity/User.java
@@ -1,6 +1,5 @@
 package com.ticketrush.boundedcontext.user.domain.entity;
 
-import com.ticketrush.boundedcontext.user.domain.types.LoginType;
 import com.ticketrush.boundedcontext.user.domain.types.UserRole;
 import com.ticketrush.global.jpa.entity.AutoIdBaseEntity;
 import jakarta.persistence.Column;
@@ -32,16 +31,11 @@ public class User extends AutoIdBaseEntity {
   @Column(name = "user_role", nullable = false)
   private UserRole role;
 
-  @Enumerated(EnumType.STRING)
-  @Column(name = "login_type", nullable = false)
-  private LoginType type;
-
   @Builder
-  public User(String name, String email, String phone, UserRole role, LoginType type) {
+  public User(String name, String email, String phone, UserRole role) {
     this.name = name;
     this.email = email;
     this.phone = phone;
     this.role = role;
-    this.type = type;
   }
 }
