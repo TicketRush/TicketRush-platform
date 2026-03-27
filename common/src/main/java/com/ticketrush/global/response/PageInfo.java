7c7437cb58c71cd8b63d9833c9b6473caafbe431
sohee52 <wneh2023@naver.com>
2026-03-27 14:27:42 +0900

    [Chore] #48 currentPage에서 pageIndex로 필드 이름 변경

--- common/src/main/java/com/ticketrush/global/response/PageInfo.java
+++ common/src/main/java/com/ticketrush/global/response/PageInfo.java
@@ -1,5 +1,4 @@
 package com.ticketrush.global.response;
 
-public record PageInfo(
-    int currentPage, int size, boolean hasNext, long totalElements, int totalPages)
+public record PageInfo(int pageIndex, int size, boolean hasNext, long totalElements, int totalPages)
     implements PaginationInfo {}
