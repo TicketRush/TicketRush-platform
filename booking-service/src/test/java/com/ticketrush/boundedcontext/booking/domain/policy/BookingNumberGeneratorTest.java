82967ffad696d8bb8b8a75d53e7e779516ec1529
sohee52 <wneh2023@naver.com>
2026-04-01 03:08:56 +0900

    [Chore] #30 컨벤션에 맞게 코드 스타일 수정

--- booking-service/src/test/java/com/ticketrush/boundedcontext/booking/domain/policy/BookingNumberGeneratorTest.java
+++ booking-service/src/test/java/com/ticketrush/boundedcontext/booking/domain/policy/BookingNumberGeneratorTest.java
@@ -2,8 +2,6 @@ package com.ticketrush.boundedcontext.booking.domain.policy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.HashSet;
-import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
