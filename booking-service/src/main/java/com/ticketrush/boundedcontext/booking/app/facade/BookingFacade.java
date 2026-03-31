9ad8b9e78fb4d885098f80d8d3befe1b2766d14d
sohee52 <wneh2023@naver.com>
2026-04-01 03:07:43 +0900

    [Refactor] #30 여러 UseCase를 조합하여 하나의 완결된 비즈니스 흐름을 만들도록 BookingFacade 수정

--- booking-service/src/main/java/com/ticketrush/boundedcontext/booking/app/facade/BookingFacade.java
+++ booking-service/src/main/java/com/ticketrush/boundedcontext/booking/app/facade/BookingFacade.java
@@ -2,6 +2,7 @@ package com.ticketrush.boundedcontext.booking.app.facade;
 
 import com.ticketrush.boundedcontext.booking.app.usecase.BookingCreateUseCase;
 import com.ticketrush.boundedcontext.booking.app.usecase.BookingIssueNumberUseCase;
+import com.ticketrush.boundedcontext.booking.domain.entity.Booking;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +13,11 @@ public class BookingFacade {
   private final BookingIssueNumberUseCase bookingIssueNumberUseCase;
   private final BookingCreateUseCase bookingCreateUseCase;
 
-  public String issueNumber() {
-    return bookingIssueNumberUseCase.execute();
-  }
+  public Booking createBooking(Long userId, Long performanceId, Long seatId, Long price) {
+    // 1. 고유 예약 번호 발급 (Redis 통신 - 트랜잭션 외부)
+    String bookingNumber = bookingIssueNumberUseCase.execute();
 
-  public void createBooking(
-      Long userId, Long performanceId, Long seatId, Long price, String bookingNumber) {
-    bookingCreateUseCase.execute(userId, performanceId, seatId, price, bookingNumber);
+    // 2. 예약 정보 DB 저장 (DB 트랜잭션 내부)
+    return bookingCreateUseCase.execute(userId, performanceId, seatId, price, bookingNumber);
   }
 }
