9437d49108f2c1c22ea5fc7318a916c163edaa1e
50h33 <wneh2023@naver.com>
2026-04-03 18:58:50 +0900

    [Chore] #30 임시 예매 상태 이름을 HOLD로 변경

--- seat-service/src/main/java/com/ticketrush/boundedcontext/seat/domain/types/SeatStatus.java
+++ seat-service/src/main/java/com/ticketrush/boundedcontext/seat/domain/types/SeatStatus.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SeatStatus {
   AVAILABLE("예매 가능"),
-  HOLDING("임시 예매"), // 사용자가 선택해서 5분간 임시로 선점된 상태
+  HOLD("임시 예매"), // 사용자가 선택해서 5분간 임시로 선점된 상태
   SOLD("예매 완료"); // 결제가 완전히 끝나서 확정된 상태
 
   private final String description;
