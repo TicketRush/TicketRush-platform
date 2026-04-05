08b671542cfcc9f9742082b7a3e6c11ee4f7b363
sohee52 <wneh2023@naver.com>
2026-04-05 14:18:39 +0900

    [Refactor] #36 BookingFacade 비동기 이벤트 발행 흐름으로 변경

--- booking-service/src/main/java/com/ticketrush/boundedcontext/booking/app/facade/BookingFacade.java
+++ booking-service/src/main/java/com/ticketrush/boundedcontext/booking/app/facade/BookingFacade.java
@@ -4,6 +4,8 @@ import com.ticketrush.boundedcontext.booking.app.dto.request.BookingCreateReques
 import com.ticketrush.boundedcontext.booking.app.usecase.BookingCreateUseCase;
 import com.ticketrush.boundedcontext.booking.app.usecase.BookingIssueNumberUseCase;
 import com.ticketrush.boundedcontext.booking.domain.entity.Booking;
+import com.ticketrush.global.eventpublisher.EventPublisher;
+import com.ticketrush.shared.booking.event.BookingCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +15,22 @@ public class BookingFacade {
 
   private final BookingIssueNumberUseCase bookingIssueNumberUseCase;
   private final BookingCreateUseCase bookingCreateUseCase;
+  private final EventPublisher eventPublisher;
 
   public Booking createBooking(Long userId, Long performanceId, Long seatId, Long price) {
-    // 1. 고유 예약 번호 발급 (Redis 통신 - 트랜잭션 외부)
+    // 1. 고유 예약 번호 발급
     String bookingNumber = bookingIssueNumberUseCase.execute();
 
-    // 2. 파라미터들을 묶어 DTO(Record) 객체 생성
+    // 2. PENDING 상태로 예매 생성
     BookingCreateRequest request =
         new BookingCreateRequest(userId, performanceId, seatId, price, bookingNumber);
+    Booking savedBooking = bookingCreateUseCase.execute(request);
 
-    // 3. 예약 정보 DB 저장 (DB 트랜잭션 내부) - 생성한 DTO를 전달
-    return bookingCreateUseCase.execute(request);
+    // 3. 트랜잭션 성공 후 "가주문 생성됨" 이벤트 발행
+    BookingCreatedEvent event =
+        new BookingCreatedEvent(savedBooking.getId(), seatId, performanceId, userId);
+    eventPublisher.publish(event);
+
+    return savedBooking;
   }
 }
