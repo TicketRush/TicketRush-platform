44b695889725f20f4c8900f5bf5b8b70634a4ca2
sohee52 <wneh2023@naver.com>
2026-04-05 15:38:15 +0900

    [Refactor] #36 존재하지 않는 bookingId일 경우 경고만 나타내도록 수정

--- booking-service/src/main/java/com/ticketrush/boundedcontext/booking/app/usecase/BookingCancelUseCase.java
+++ booking-service/src/main/java/com/ticketrush/boundedcontext/booking/app/usecase/BookingCancelUseCase.java
@@ -2,8 +2,6 @@ package com.ticketrush.boundedcontext.booking.app.usecase;
 
 import com.ticketrush.boundedcontext.booking.domain.entity.Booking;
 import com.ticketrush.boundedcontext.booking.out.repository.BookingRepository;
-import com.ticketrush.global.exception.BusinessException;
-import com.ticketrush.global.status.ErrorStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,17 +16,15 @@ public class BookingCancelUseCase {
   private final BookingRepository bookingRepository;
 
   public void execute(Long bookingId) {
-    Booking booking =
-        bookingRepository
-            .findById(bookingId)
-            .orElseThrow(
-                () -> {
-                  log.warn("예매 취소 실패. 존재하지 않는 bookingId={}", bookingId);
-                  return new BusinessException(ErrorStatus.BOOKING_NOT_FOUND);
-                });
+    Booking booking = bookingRepository.findById(bookingId).orElse(null);
+
+    if (booking == null) {
+      log.warn("보상 트랜잭션 스킵. 존재하지 않는 bookingId={}", bookingId);
+      return;
+    }
 
     booking.cancel();
 
-    log.debug("보상 트랜잭션 정상 처리: 예매가 취소(CANCELED) 되었습니다. bookingId: {}", bookingId);
+    log.debug("보상 트랜잭션 정상 처리 : 예매가 취소(CANCELED) 되었습니다. bookingId: {}", bookingId);
   }
 }
