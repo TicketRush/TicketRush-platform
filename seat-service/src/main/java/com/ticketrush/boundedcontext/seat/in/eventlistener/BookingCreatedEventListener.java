d8dcefc42406d04a90448750fae0449f6d0d7590
50h33 <wneh2023@naver.com>
2026-04-07 16:06:37 +0900

    [Refactor] #36 좌석 선점 성공 시 ack를 호출

--- seat-service/src/main/java/com/ticketrush/boundedcontext/seat/in/eventlistener/BookingCreatedEventListener.java
+++ seat-service/src/main/java/com/ticketrush/boundedcontext/seat/in/eventlistener/BookingCreatedEventListener.java
@@ -7,6 +7,7 @@ import com.ticketrush.shared.booking.event.BookingCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -19,10 +20,15 @@ public class BookingCreatedEventListener {
   private final JsonConverter jsonConverter;
 
   @KafkaListener(topics = "booking-created-topic", groupId = "seat-group")
-  public void handleBookingCreated(@Payload DomainEventEnvelope envelope) {
+  public void handleBookingCreated(@Payload DomainEventEnvelope envelope, Acknowledgment ack) {
+
     BookingCreatedEvent event =
         jsonConverter.deserialize(envelope.payload(), BookingCreatedEvent.class);
 
+    // 1. 좌석 선점 시도 (실패하거나 예외가 터지면 상위로 전파됨)
     seatFacade.tryLockSeat(event.bookingId(), event.seatId(), event.userId());
+
+    // 2. 예외 없이 로직이 성공적으로 완료되었을 때만 오프셋 수동 커밋
+    ack.acknowledge();
   }
 }
