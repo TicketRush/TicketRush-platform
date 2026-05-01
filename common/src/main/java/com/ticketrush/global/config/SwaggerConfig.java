515d8d97d2a935496542dad58bb6f0443d9b21bb
민주's mac <calla20031@Mac.local>
2026-05-01 20:20:20 +0900

    [Fix] #138 SwaggerConfig 중복 클래스 제거 및 common 모듈 포트 동적화

--- common/src/main/java/com/ticketrush/global/config/SwaggerConfig.java
+++ common/src/main/java/com/ticketrush/global/config/SwaggerConfig.java
@@ -2,15 +2,19 @@ package com.ticketrush.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+  @Value("${server.port:8080}")
+  private int serverPort;
+
   @Bean
   public OpenAPI openAPI() {
-    return new OpenAPI().servers(List.of(new Server().url("http://localhost:8080")));
+    return new OpenAPI()
+        .addServersItem(new Server().url("http://localhost:" + serverPort).description("로컬 서버"));
   }
 }
