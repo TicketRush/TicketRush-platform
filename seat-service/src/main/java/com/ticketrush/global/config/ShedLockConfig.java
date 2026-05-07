617ff14ea43b5616da7e019318a87e331ad4e630
50h33 <wneh2023@naver.com>
2026-05-07 14:43:48 +0900

    [Refactor] #172 환경에 따라 키가 겹치지 않도록 ShedLock의 prefix를 환경별로 분리

--- seat-service/src/main/java/com/ticketrush/global/config/ShedLockConfig.java
+++ seat-service/src/main/java/com/ticketrush/global/config/ShedLockConfig.java
@@ -3,6 +3,7 @@ package com.ticketrush.global.config;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,9 +12,16 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 @EnableSchedulerLock(defaultLockAtMostFor = "1m")
 public class ShedLockConfig {
 
+  @Value("${spring.application.name:seat-service}")
+  private String applicationName;
+
+  @Value("${spring.profiles.active:default}")
+  private String activeProfile;
+
   @Bean
   public LockProvider lockProvider(RedisConnectionFactory connectionFactory) {
-    // 환경에 따라 키가 겹치지 않도록 prefix를 지정합니다.
-    return new RedisLockProvider(connectionFactory, "seat-env");
+    // 환경에 따라 키가 겹치지 않도록 동적으로 prefix 지정 (예: "seat-service-dev")
+    String keyPrefix = applicationName + "-" + activeProfile;
+    return new RedisLockProvider(connectionFactory, keyPrefix);
   }
 }
