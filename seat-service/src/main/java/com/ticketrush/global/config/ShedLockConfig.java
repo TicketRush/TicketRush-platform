f285a1351cbe3c82c7019a2c4fd907974348bd75
50h33 <wneh2023@naver.com>
2026-05-07 15:08:14 +0900

    [Refactor] #172 @EnableScheduling 위치 변경

--- seat-service/src/main/java/com/ticketrush/global/config/ShedLockConfig.java
+++ seat-service/src/main/java/com/ticketrush/global/config/ShedLockConfig.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableSchedulerLock(defaultLockAtMostFor = "1m")
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "55s")
 public class ShedLockConfig {
 
   @Value("${spring.application.name:seat-service}")
